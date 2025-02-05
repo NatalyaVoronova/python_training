d5f413205439d9dc764bea6649529cab92923de4
NatalyaVoronova <loginova2024.1@gmail.com>
2025-02-05 18:15:35 +0500

    Выделены вспомогательные методы в тесте по добавлению нового контакта, создан класс Contact.

---
+++ contact.py
@@ -0,0 +1,19 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone, mobile_phone, work, fax, email1, email2, email3, home_articles):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work = work
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.home_articles = home_articles
+
