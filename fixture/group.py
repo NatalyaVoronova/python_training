005df521ab9e5cfd5715fa6849d80a191b60c09e
NatalyaVoronova <loginova2024.1@gmail.com>
2025-02-22 16:57:54 +0500

    Создан тест на изменение группы.

--- fixture/group.py
+++ fixture/group.py
@@ -30,9 +30,28 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         # select first group
-        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("selected[]").click()  # первый элемент списка групп
         # submit deletion
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def modify_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit modify
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("Group's name is New")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("New header")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("New footer description")
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
 
