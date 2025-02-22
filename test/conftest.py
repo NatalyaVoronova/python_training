edf5adbe26eb0fbc105c1ab2a3896fef0bba1b7f
NatalyaVoronova <loginova2024.1@gmail.com>
2025-02-22 15:11:21 +0500

    Создан тест на удаление "группы". Создан файл единой фикстуры для всех тестов (conftest.py).

---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from fixture.application import Application
+
+
+#@pytest.fixture() # фикстура создается для каждого теста
+@pytest.fixture(scope="session") # фикстура создается одна для всех тестов
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
