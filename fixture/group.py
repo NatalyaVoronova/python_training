d68fd7f99e3ff103906adef26d3f635cebbb51b9
NatalyaVoronova <loginova2024.1@gmail.com>
2025-02-28 08:16:25 +0500

    Доработка теста edit_group + new_group_data

--- fixture/group.py
+++ fixture/group.py
@@ -40,21 +40,15 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def edit_first_group(self, name, header, footer):  # до модификации - до использования new_group_data
+    def edit_first_group(self, new_group_data):  # ранее использовался без new_group_data
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
-        # submit modify
+        # open modification form
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-#from model.group import Group
+from model.group import Group
 import datetime
 
+
 def test_edit_first_group(app):
-    app.group.edit_first_group("Optimus Prime " + str(datetime.datetime.now().time()), "header", "footer description")
-
-
+    app.group.edit_first_group(Group(name="Optimus Prime " + str(datetime.datetime.now().time()),
+                                     header="New constellation" + str(datetime.datetime.now().time())))
