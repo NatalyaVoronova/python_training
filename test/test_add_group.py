60c8ded57eac3b76c9d3dbfe260c9152765cc807
NatalyaVoronova <loginova2024.1@gmail.com>
2025-03-09 00:25:12 +0500

    Добавлена проверка на длину списков контактов (L 4.8).

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="Group name is New 36", header="This 36", footer="mushrooms 36"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,5 +6,8 @@ import datetime
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="Optimus Prime " + str(datetime.datetime.now().time()),
                                      header="New constellation" + str(datetime.datetime.now().time())))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
