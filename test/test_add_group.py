2ae5ef5569961d8aec6f357e51fc7ffafb6a0818
NatalyaVoronova <loginova2024.1@gmail.com>
2025-03-10 00:36:54 +0500

    Добавлено кеширование и хеширование (L 4_11).

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,23 +2,21 @@
 from model.group import Group
 
 
-
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="Group name is New 36", header="This 36", footer="mushrooms 36")
     app.group.create(group)
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(name="", header="", footer="")
+#     app.group.create(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
