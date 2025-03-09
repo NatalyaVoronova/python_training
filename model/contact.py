40aa9e104d721e1c7c21041cd5254803fb968cab
NatalyaVoronova <loginova2024.1@gmail.com>
2025-03-09 17:31:17 +0500

    Добавлена проверка в тесте удаления контактов на соответствие списков (DZ 10 step 2).

--- model/contact.py
+++ model/contact.py
@@ -27,3 +27,9 @@ class Contact:
         self.ayear = ayear
         self.new_group = new_group
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
