ca5267c28120d964791e0485b06aeeb518f58ce8
NatalyaVoronova <loginova2024.1@gmail.com>
2025-03-11 10:52:48 +0500

    Добавлен рандомный выбор индекса группа для модификации или удаления(L 4_12).

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from random import randrange
 import datetime
 
 
@@ -7,11 +8,12 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="Optimus Prime " + str(datetime.datetime.now().time()),
                   header="New constellation" + str(datetime.datetime.now().time()))
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == app.group.count()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
